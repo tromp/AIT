de7d6eb9699a3f6b172cd99495bf5839ed530d6c
John Tromp <john.tromp@gmail.com>
2015-07-24 17:06:45 -0400

    add some fun python

--- Primes.hs
+++ Primes.hs
@@ -1,3 +1,6 @@
+import Control.Monad
+import Control.Monad.Fix
+
 -- nubBy(((>1).).gcd)[2..]
 
 primes = let
@@ -7,7 +10,14 @@ primes = let
   primes n = True:f (primes sn) where
     f = sn f
     sn = succ n
-in False:False:primes zero
+ in False:False:primes zero
 
 main=print.take 30$
-let f='.';o c(x:y)=x:c y;z c(x:y)=f:c y;p n='p':ap fix p(o.n)in f:f:p z
+ let
+ f='.'
+ o c(x:y)=x:c y
+ z c(x:y)=f:c y
+ p n='p':ap fix p(o.n)
+ in f:f:p z
+
+--let f='.';o c(x:y)=x:c y;z c(x:y)=f:c y;p n='p':ap fix p(o.n)in f:f:p z
---
+++ lambda.py
@@ -0,0 +1,9 @@
+import sys
+def c(j,t):
+ sys.stdout.write(j('.')('P'))
+ return t
+(lambda z:lambda y:z(z(y(lambda p:lambda n:(lambda s:lambda z:z(lambda x:
+lambda y:y)(lambda d:p(s)(y(s))(d)))(lambda x:lambda a:lambda s:lambda p:
+p(a)(lambda y:s(n(x))(y))))(lambda c:lambda a:lambda s:z(lambda y:s(c)(y)
+))))(y(lambda p:lambda b:lambda t:t(c(b,p)))))(lambda s:lambda p:p(lambda
+x:lambda y:x)(s))(lambda f:(lambda q:q(q))(lambda x:f(lambda y:x(x)(y))))
