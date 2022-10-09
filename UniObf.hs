56c30edd96615a1a6b605b2b7d728c74c502e3c6
John Tromp <john.tromp@gmail.com>
2022-10-09 20:47:24 +0200

    minimize UniObf.hs some more:)

--- UniObf.hs
+++ UniObf.hs
@@ -2,15 +2,15 @@ import Control.Applicative hiding((<|>),many)
 import Text.Parsec
 import System.IO
 data W=S!Char|F(W->W)
-j(F f)=f
+(F f)&w=f w
 f=c$F id
 c=F .const
-a x y v=x v`j`y v
+a x y v=x v&y v
 l e v=F$ \a->e(a:v)
-q x y=F$ \z->z`j`x`j`y
+q x y=F$ \z->z&x&y
 b n=if even n then F c else f
-s w=c where(S c)=w`j`S '0'`j`S '1'
-o p=map s.g.(p[]`j`).foldr(q.b.fromEnum)f
-g l=case (l`j`c(c.S$':'))of{S ':'->l`j`F c:g(l`j`f);F _->[]}
+s w=c where(S c)=w&S '0'&S '1'
+o p=map s.g.(p[]&).foldr(q.b.fromEnum)f
+g l=case (l&c(c.S$':'))of{S ':'->l&F c:g(l&f);F _->[]}
 x=char '0'*>(l<$char '0'<*>x<|>a<$char '1'<*>x<*>x)<|>flip(!!)<$>pred.length<$>many(char '1')<*char '0'
 main=hSetBuffering stdout NoBuffering>>interact(either(error.show)id.parse(o<$>x<*>getInput)"")
