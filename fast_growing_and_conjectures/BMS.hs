87b2fad5d36aeaa2a239ef5015d72abf3b333e31
John Tromp <john.tromp@gmail.com>
2024-08-13 17:33:15 +0200

    calling with rows instead of pred rows is ok

--- fast_growing_and_conjectures/BMS.hs
+++ fast_growing_and_conjectures/BMS.hs
@@ -7,13 +7,9 @@ import Data.List
 
 type Matrix = [Int]
 
--- proper mod
-pmod :: Int -> Int -> Int
-pmod a b = ((a `mod` b) + b) `mod` b
-
 showbms :: Int -> [Int] -> String
 showbms rows mat = concat $ map (row (pad mat)) [rows-1, rows-2..0] where
-  pad mat = replicate ((rows - length mat) `pmod` rows) 0 ++ mat
+  pad mat = replicate ((rows - length mat) `mod` rows) 0 ++ mat
   row mat r = go (drop r mat) where
     go []    = "0\n"
     go (n:m) = show n ++ "\t" ++ go (drop (rows-1) m)
@@ -27,7 +23,7 @@ expand mod mat@(m1:m) = pre0 ++ m where
 matrix0 n = map (const n) [1..n]    -- single column of n n's
 
 bms :: Int -> [[Int]]
-bms rows = go (rows-1) (matrix0 rows) where
+bms rows = go rows (matrix0 rows) where
   go mod  [] = []
   go mod mat = mat : go (if mod==0 then rows-1 else pred mod) (expand mod mat)
 
@@ -35,5 +31,6 @@ bmsize :: Int -> [[Int]] -> String
 bmsize rows [] = show rows
 bmsize rows (m:ms) = show rows ++ "^(" ++ bmsize rows ms ++ ")^" ++ show (length m)
 
+main = mapM_ print $ bms 2
 -- main = mapM_ (putStrLn . showbms 2) $ bms 2
-main = putStrLn . bmsize 2 . bms $ 2
+-- main = putStrLn . bmsize 2 . bms $ 2
