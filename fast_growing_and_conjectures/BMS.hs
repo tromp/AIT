dea81c62c6144dccb11d0b107d0f86ccc56b0fcb
John Tromp <john.tromp@gmail.com>
2024-08-13 23:06:06 +0200

    stop on initial 0 in array rather than empty array

--- fast_growing_and_conjectures/BMS.hs
+++ fast_growing_and_conjectures/BMS.hs
@@ -20,11 +20,11 @@ expand mod mat@(m1:m) = pre0 ++ m where
   pre1 = take (fromIntegral m1) (m ++ [0,0..])
   pre0 = map (\(i,mi) -> mi + if mi+i >= m1+mod then m1 else 0) (zip [0..] pre1)
 
-matrix0 n = map (const n) [1..n]    -- single column of n n's
+matrix0 n = map (const n) [1..n^n]    -- single column of n n's
 
 bms :: Int -> [[Int]]
-bms rows = go rows (matrix0 rows) where
-  go mod  [] = []
+bms rows = go (rows-1) (matrix0 rows) where
+  go mod (0:_) = []
   go mod mat = mat : go (if mod==0 then rows-1 else pred mod) (expand mod mat)
 
 bmsize :: Int -> [[Int]] -> String
@@ -34,3 +34,6 @@ bmsize rows (m:ms) = show rows ++ "^(" ++ bmsize rows ms ++ ")^" ++ show (length
 main = mapM_ print $ bms 2
 -- main = mapM_ (putStrLn . showbms 2) $ bms 2
 -- main = putStrLn . bmsize 2 . bms $ 2
+
+-- BMS 2 = 2^(2^(2^(2^(2^(2^(2^(2^(2^(2^(2^(2^(2^(2^(2^(2^2^57)^56)^53)^50)^49)^46)^41)^36)^29)^22)^21)^14)^11)^6)^5)^4
+--       > 2^^20
