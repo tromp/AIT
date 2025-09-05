4d02a1f76a300bfad26404bfc6e846b7aa39197b
John Tromp <john.tromp@gmail.com>
2025-09-05 23:12:21 +0200

    reduce from 404 to 350 bits

--- fast_growing_and_conjectures/BMS.hs
+++ fast_growing_and_conjectures/BMS.hs
@@ -1,39 +1,44 @@
--- https://arxiv.org/pdf/2307.04606
+-- https://arxiv.org/abs/2307.04606
 -- Well-Orderedness of the Bashicu Matrix System
--- Samuel Vargovˇc´ık
+-- Rachel Hunter
 -- July 12, 2023
+-- this program implements the Flattened Address Matrix System (FAMS) variant described in
+-- https://codegolf.stackexchange.com/questions/18028/largest-number-printable/273656#273656
+-- by user https://codegolf.stackexchange.com/users/101119/patcail
+-- except that addresses run in reverse (parent addresses are always larger than their children)
+-- and elements don't contain the address of their parent, but the distance to their parent
+-- So it should perhaps be called Flattened Distance Matrix System (FDMS)
 
 import Data.List
 
 type Matrix = [Int]
 
 showbms :: Int -> [Int] -> String
-showbms rows mat = concat $ map (row (pad mat)) [rows-1, rows-2..0] where
+showbms rows mat = concat (map (row (pad mat)) [rows-1, rows-2..0]) where
   pad mat = replicate ((rows - length mat) `mod` rows) 0 ++ mat
   row mat r = go (drop r mat) where
-    go []    = "0\n"
+    go []    = "\n"
     go (n:m) = show n ++ "\t" ++ go (drop (rows-1) m)
 
 expand :: Int -> [Int] -> [Int]
 expand mod [] = []
 expand mod mat@(m1:m) = pre0 ++ m where
-  pre1 = take (fromIntegral m1) (m ++ [0,0..])
+  pre1 = take (fromIntegral m1) m
   pre0 = map (\(i,mi) -> mi + if mi+i >= m1+mod then m1 else 0) (zip [0..] pre1)
 
-matrix0 n = map (const n) [1..n^n]    -- single column of n n's
+-- 2 columns of n n's followed by a column of n 0s
+matrix0 n = map (const n) [1..2*n] ++ map (const 0) [1..n]
 
+-- return list of iteratively expanded matrices up to one with 0 head
 bms :: Int -> [[Int]]
 bms rows = go (rows-1) (matrix0 rows) where
-  go mod (0:_) = []
+  go mod mat@(0:_) = [mat]
   go mod mat = mat : go (if mod==0 then rows-1 else pred mod) (expand mod mat)
 
-bmsize :: Int -> [[Int]] -> String
-bmsize rows [] = show rows
-bmsize rows (m:ms) = show rows ++ "^(" ++ bmsize rows ms ++ ")^" ++ show (length m)
+bmsize :: [[Int]] -> Int
+bmsize = sum . map head
 
-main = mapM_ print $ bms 2
--- main = mapM_ (putStrLn . showbms 2) $ bms 2
--- main = putStrLn . bmsize 2 . bms $ 2
-
--- BMS 2 = 2^(2^(2^(2^(2^(2^(2^(2^(2^(2^(2^(2^(2^(2^(2^(2^2^57)^56)^53)^50)^49)^46)^41)^36)^29)^22)^21)^14)^11)^6)^5)^4
---       > 2^^20
+main = do
+  mapM_ print $ bms 2
+  mapM_ (putStrLn . showbms 2) $ bms 2
+  print . bmsize . bms $ 2
